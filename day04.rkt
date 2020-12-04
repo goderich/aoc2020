110641ef1d456c6c9539a898c0c078808ce00d35
Goderich <yuhsien77@gmail.com>
2020-12-04 16:28:25 +0800

    Rewrite day 4 using parser combinators

--- day04.rkt
+++ day04.rkt
@@ -1,37 +1,46 @@
 #lang racket
 
+(require megaparsack
+         megaparsack/text
+         data/monad
+         data/applicative
+         data/either)
+
 (define (valid1? pass-str)
   (define passport (string-split pass-str #px"(\\s+|\n)"))
   (define fields (map (λ (s) (first (string-split s ":"))) passport))
   (for/and ((fld '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid")))
     (member fld fields)))
 
-;; part 1
-
 (define part1-list
   (filter valid1? (string-split (file->string "inputs/day04.txt") "\n\n")))
+
+;; part 1
 
 (length part1-list)
 
 ;; part 2
 
-(define (valid-hgt? hgt)
-  (define num
-    (string->number
-     (list->string
-      (takef (string->list hgt) char-numeric?))))
-  (cond
-    ((string-suffix? hgt "cm") (>= 193 num 150))
-    ((string-suffix? hgt "in") (>= 76 num 59))
-    (else #f)))
+(define hgt/p
+  (do (num <- integer/p)
+      (hgt-unit <- (or/p (string/p "cm")
+                         (string/p "in")))
+      (pure (cons num hgt-unit))))
 
-(define (valid-hcl? hcl)
-  (define chars (string->list hcl))
-  (and (eq? #\# (first chars))
-       (for/and ((c (rest chars)))
-         (or (char-numeric? c)
-             (member c '(#\a #\b #\c #\d #\e #\f))))
-       (= 7 (length chars))))
+(define (valid-hgt? hgt)
+  (define result
+    (from-success #f (parse-string hgt/p hgt)))
+  (match result
+    ((cons num "cm") (>= 193 num 150))
+    ((cons num "in") (>= 76 num 59))
+    (_ #f)))
+
+(define hcl/p
+  (do (char/p #\#)
+      (repeat/p 6
+       (or/p digit/p
+             (char-between/p #\a #\f)))
+      (pure 'ok)))
 
 (define (valid2? str)
   (define passport (string-split str #px"(\\s+|\n)"))
@@ -43,7 +52,7 @@
       ("iyr" (>= 2020 (string->number (second fld)) 2010))
       ("eyr" (>= 2030 (string->number (second fld)) 2020))
       ("hgt" (valid-hgt? value))
-      ("hcl" (valid-hcl? value))
+      ("hcl" (success? (parse-string hcl/p value)))
       ("ecl" (member value '("amb" "blu" "brn" "gry" "grn" "hzl" "oth")))
       ("pid" (and (= 9 (string-length value))
                   (for/and ((c (in-string value))) (char-numeric? c))))
