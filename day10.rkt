cb4f2e2945db3baeb088682bcd8883a826987847
Goderich <yuhsien77@gmail.com>
2020-12-10 20:08:47 +0800

    Finalize day 10

--- day10.rkt
+++ day10.rkt
@@ -10,20 +10,25 @@
          (cons max-joltage)
          (sort _ <))))
 
-(define diffs
+(define distances
   (map -
        (rest jolts)
        (drop-right jolts 1)))
 
-(* (count (curry = 1) diffs)
-   (count (curry = 3) diffs))
+(* (count (curry = 1) distances)
+   (count (curry = 3) distances))
 
-(define (skippable? n)
-  (define prev (list-ref jolts (sub1 n)))
-  (define next (list-ref jolts (add1 n)))
-  ((- next prev) . <= . 3))
+(define (consecutive-ones lst)
+  (for/fold ((num-ones 0)
+             (acc '())
+             #:result (reverse acc))
+            ((x lst))
+    (cond
+      ((= 1 x) (values (add1 num-ones) acc))
+      ((= 0 num-ones) (values num-ones acc))
+      (else (values 0 (cons num-ones acc))))))
 
-(count skippable? (range 1 (sub1 (length jolts))))
-
-;; '(1 (6 7 8) 17 18 26 27 (35 36 37) (45 46 47) 52 60 61 66 67 (72 73 74) 88 89 94 (103 104 105) (110 111 112) 117 122 123 (134 135 136) 141 (146 147 148) 160 (169 170 171) (179 180 181))
-(* 2 7 2 2 2 2 7 7 2 2 2 2 2 7 2 2 2 7 7 2 2 2 7 2 7 2 7 7)
+(~>> distances
+     (consecutive-ones)
+     (map (λ (x) (add1 (apply + (range x)))))
+     (apply *))
