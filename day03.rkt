4b5b522f3fec771800e9fddc13f31fdd205625bc
Goderich <yuhsien77@gmail.com>
2020-12-14 22:19:15 +0800

    Add comments to day 3

--- day03.rkt
+++ day03.rkt
@@ -1,6 +1,12 @@
 #lang racket
 
+;; Here we use a function for both parts. Since the value of "down" is 1
+;; in all cases but one, we can have it as the default value.
 (define (numtrees right (down 1))
+  ;; We read in the list here, to separate it from the collision checks
+  ;; in the second fold. If "down" equals 1, it returns the full list, but
+  ;; if it's 2, it returns only the even lines. Then we can iterate over
+  ;; the resulting list below without worrying about "down" any longer.
   (define rows
     (for/list ((line (file->lines "inputs/day03.txt"))
                (y-coord (in-naturals))
@@ -13,11 +19,11 @@
         (add1 acc)
         acc)))
 
-;; part 1
+;; Answer to part 1.
 
 (numtrees 3)
 
-;; part 2
+;; Answer to part 2.
 
 (* (numtrees 1)
    (numtrees 3)
