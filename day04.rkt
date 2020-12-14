7f072502ef79943acfe7742c2807b6086e87de1f
Goderich <yuhsien77@gmail.com>
2020-12-14 22:14:34 +0800

    Add comments to day 4

--- day04.rkt
+++ day04.rkt
@@ -1,32 +1,45 @@
 #lang racket
 
+;; This day is well suited for parser combinators. All the imports below
+;; are needed for parser combinators from the Megaparsack library.
 (require megaparsack
          megaparsack/text
          data/monad
          data/applicative
          data/either)
 
+;; Part 1
+;;
+;; The first function only determines that each passport has all the
+;; required fields.
 (define (valid1? pass-str)
   (define passport (string-split pass-str #px"(\\s+|\n)"))
   (define fields (map (λ (s) (first (string-split s ":"))) passport))
   (for/and ((fld '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid")))
     (member fld fields)))
 
-(define part1-list
+;; We read the input, throwing out the invalid passports.
+(define passport-list
   (filter valid1? (string-split (file->string "inputs/day04.txt") "\n\n")))
 
-;; part 1
+;; Answer to part 1.
+(length passport-list)
 
-(length part1-list)
+;; Part 2
+;;
+;; In part 2, I use parser combinators. They allow me to precisely define
+;; the strings I'm parsing, extract values from them, and combine parsers.
 
-;; part 2
-
+;; Parser for height values.
 (define hgt/p
   (do (num <- integer/p)
       (hgt-unit <- (or/p (string/p "cm")
                          (string/p "in")))
+      eof/p
       (pure (cons num hgt-unit))))
 
+;; This function returns #t iff the height value is correctly formatted
+;; and within the accepted range.
 (define (valid-hgt? hgt)
   (define result
     (from-success #f (parse-string hgt/p hgt)))
@@ -35,6 +48,9 @@
     ((cons num "in") (>= 76 num 59))
     (_ #f)))
 
+;; Hair colour parser. Here we only care about the format and not the value.
+;; If the format is correct, it returns (success 'ok), which we can then
+;; test with the `success?` predicate.
 (define hcl/p
   (do (char/p #\#)
       (repeat/p 6
@@ -43,11 +59,13 @@
       eof/p
       (pure 'ok)))
 
+;; PID parser. Here we only care about the format and not the value.
 (define pid/p
   (do (repeat/p 9 digit/p)
       eof/p
       (pure 'ok)))
 
+;; This function checks if a passport is valid according to part 2 rules.
 (define (valid2? str)
   (define passport (string-split str #px"(\\s+|\n)"))
   (define fields (map (λ (s) (string-split s ":")) passport))
@@ -63,4 +81,5 @@
       ("pid" (success? (parse-string pid/p value)))
       ("cid" #t))))
 
-(count valid2? part1-list)
+;; Answer to part 2.
+(count valid2? passport-list)
