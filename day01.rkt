1c7310028e424c272244360fd82323d274d10e4b
Goderich <yuhsien77@gmail.com>
2020-12-23 23:21:38 +0800

    Add day 1 comments

--- day01.rkt
+++ day01.rkt
@@ -2,17 +2,20 @@
 
 (require racket/file)
 
-;; part 1
-
+;; We begin by reading all the numbers into a list of ints.
 (define nums (map string->number (file->lines "inputs/day01.txt")))
 
+;; Racket's looping tools are an excellent match for this task. Here we
+;; have a nested loop where we are looking for the first occurrence of
+;; variables that satisfy a certain condition: a perfect match for the
+;; `for*/first` loop with the `#:when` keyword.
 (for*/first ((i nums)
              (j nums)
              #:when (= 2020 (+ i j)))
     (* i j))
 
-;; part 2
-
+;; For part 2 we just add a third variable to the loop, condition, and
+;; answer.
 (for*/first ((i nums)
              (j nums)
              (k nums)
